5b688732a54a1e099c517aa59d3184f788b153d5
Luis Hau <ycluisdev@gmail.com>
2023-05-29 09:57:54 +0800

    Exercises 3.7.-3.8.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const PORT = 3001
 
 app.use(express.json())
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+      tokens.body(req, res),
+    ].join(' ')
+  }),
+)
 
 let persons = [
   {
