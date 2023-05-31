18aebc0ec9f1eb78abc6b8f3860ea6e6f549404a
Luis Hau <luis.hau@asiapacificex.com>
2023-05-31 12:21:46 +0800

    Exercises 4.8.-4.12.

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,23 +1,30 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (req, res, next) => {
-  Blog.find({})
-    .then((blogs) => {
-      res.json(blogs)
-    })
-    .catch((err) => next(err))
+blogRouter.get('/', async (req, res, next) => {
+  try {
+    const blogs = await Blog.find({})
+    res.status(200).json(blogs)
+  } catch (err) {
+    next(err)
+  }
 })
 
-blogRouter.post('/', (req, res, next) => {
-  const blog = new Blog(req.body)
+blogRouter.post('/', async (req, res, next) => {
+  try {
+    const { title, author, url, likes } = req.body
 
-  blog
-    .save()
-    .then((result) => {
-      res.status(201).json(result)
+    const blog = new Blog({
+      title,
+      author,
+      url,
+      likes: likes || 0,
     })
-    .catch((err) => next(err))
+    const response = await blog.save()
+    res.status(201).json(response)
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = blogRouter
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
+const { initialBlog } = require('../utils/list_helper')
 
 const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  console.log('Before each: data cleared...')
+
+  const blogObj = initialBlog().map((blog) => new Blog(blog))
+  const promiseArr = blogObj.map((blog) => blog.save())
+  await Promise.all(promiseArr)
+
+  console.log('Before each completed..')
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -10,6 +23,66 @@ test('blogs are returned as json', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/)
 }, 8000)
+
+test('unique identifier property', async () => {
+  const blogs = await api.get('/api/blogs')
+  for (const blog of blogs.body) {
+    expect(blog.id).toBeDefined()
+  }
+}, 8000)
+
+test('create new blog', async () => {
+  const newBlog = {
+    title: 'Divergent',
+    author: 'Veronica Roth',
+    url: 'http://www.veronicarothbooks.com/',
+    likes: 30,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogs = await api.get('/api/blogs')
+  const title = blogs.body.map((blog) => blog.title)
+
+  expect(blogs.body).toHaveLength(initialBlog().length + 1)
+  expect(title).toContain(`${newBlog.title}`)
+})
+
+test('auto fill likes to 0 if missing', async () => {
+  const newBlog = {
+    title: 'Danger’s Kiss',
+    author: 'Glynnis Campbell',
+    url: 'http://www.veronicarothbooks.com/',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogs = await api.get('/api/blogs')
+  const latestBlog = blogs.body.find((blog) => blog.title === newBlog.title)
+
+  expect(latestBlog.likes).toBe(0)
+})
+
+test('response with 400 if missing properties', async () => {
+  const newBlog = {
+    title: 'Danger’s Kiss',
+    author: 'Glynnis Campbell',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
